4d56e52b7d922257e8290841ee9564417c693c2b
ArcticGizmo <jon.howell314@gmail.com>
2023-12-12 22:29:02 +0800

    Added player death and fog of war off in editor

--- Player.gd
+++ Player.gd
@@ -26,6 +26,6 @@ func kill():
 	get_tree().reload_current_scene()
 
 
-#func _on_area_2d_body_entered(body):
-#	if "Enemy" in body.name:
-#		kill()
+func _on_area_2d_body_entered(body):
+	if "Enemy" in body.name:
+		kill()
